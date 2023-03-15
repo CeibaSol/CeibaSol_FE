c01b21623557f641570a6171ccc2b372d5117d4f
DuvanJC <djcmnhoo@gmail.com>
2023-03-15 09:11:03 -0500

    Adaptative [Quienes Somos]
    posicionamientto de imagenes y descripcion de los personaes cuando la pantalla se hace pequeña

--- src/components/quienesSomos/quienesSomos.js
+++ src/components/quienesSomos/quienesSomos.js
@@ -13,61 +13,78 @@ export default function QuienesSomos() {
       <main>
         <div className="mx-auto max-w-7xl py-2 sm:px-6 lg:px-10 flex">
           <div className="px-4 py-6 sm:px-0 space-y-8">
+
             <div class="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-              <div className="flex space-x-2 justify-center xlg:grid md:grid:cols-2">
-                <div class="flex-none w-52 relative md:flex md:flex-col md:justify-center">
-                  <img src="picture1.png" alt="" class="relative inset-0 w-500 h-500 object-cover rounded-lg" loading="lazy" />
+              <div className="flex flex-col md:flex-row">
+                <div className="md:w-1/4 md:order-1 flex-auto p-6">
+                  <div class="flex-none w-52 ">
+                    <img src="picture1.png" alt="" class="relative inset-0 w-500 h-500 object-cover rounded-lg" loading="lazy" />
+                  </div>
                 </div>
-                <div class="flex-auto p-6">
+                <div className="md:w-3/4 md:order-2">
                   <div class="flex flex-wrap items-baseline">
-                    <h4 class=" mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      Perfil profesional:
-                    </h4>
-                    <h5 class="font-normal text-gray-700 dark:text-gray-400">
-                      Como ingeniera ambiental cuento con la capacidad investigativa e innovadora, frente a la evaluación, prevención y solución de problemas ambientales; regida siempre bajo los principios de un profesional íntegro, crítico y ético. Tengo un alto nivel de compromiso, liderazgo y responsabilidad con las actividades que me sean designadas, así como la facilidad de trabajar en equipos multidisciplinarios. Poseo experiencia en la ejecución, desarrollo y seguimiento de proyectos de educación ambiental con comunidades, y manejo de softwares como Excel, ArcGis, y programas para edición de videos; finalmente cuento con un buen manejo del idioma inglés en conversación, lectura, escritura y traducción.
-                    </h5>
+                    <div class="flex-auto p-6">
+                      <div class="flex flex-wrap items-baseline">
+                        <h4 class=" mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                          Perfil Profesional:
+                        </h4>
+                        <h5 class="font-normal text-gray-700 dark:text-gray-400">
+                          Como ingeniera ambiental cuento con la capacidad investigativa e innovadora, frente a la evaluación, prevención y solución de problemas ambientales; regida siempre bajo los principios de un profesional íntegro, crítico y ético. Tengo un alto nivel de compromiso, liderazgo y responsabilidad con las actividades que me sean designadas, así como la facilidad de trabajar en equipos multidisciplinarios. Poseo experiencia en la ejecución, desarrollo y seguimiento de proyectos de educación ambiental con comunidades, y manejo de softwares como Excel, ArcGis, y programas para edición de videos; finalmente cuento con un buen manejo del idioma inglés en conversación, lectura, escritura y traducción.
+                        </h5>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div class=" block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-              <div className="flex space-x-2 justify-center">
-                <div class="flex-auto p-6">
-
-                  <div class="flex flex-wrap items-baseline">
-                    <h4 class=" mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      Perfil Profesional:
-                    </h4>
-                    <h5 class="font-normal text-gray-700 dark:text-gray-400">
-                      Ingeniero Ambiental
-                      Especialista en Gerencia de Proyectos
-                      Especialista Tecnológico en Implementación de Sistemas de Información Geográfica
-                      Piloto Profesional de Aeronaves no Tripuladas - UAS/RPAS/DRONE
-                    </h5>
+            <div class="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+              <div className="flex flex-col md:flex-row">
+                <div className="md:w-1/4 md:order-2 flex-auto p-6">
+                  <div class="flex-none w-52 ">
+                    <img src="picture2.png" alt="" class="relative inset-0 w-500 h-500 object-cover rounded-lg" loading="lazy" />
                   </div>
-
                 </div>
-                <div class="flex-none w-52 relative">
-                  <img src="picture2.png" alt="" class="relative inset-0 w-500 h-500 object-cover rounded-lg" loading="lazy" />
+                <div className="md:w-3/4 md:order-1">
+                  <div class="flex flex-wrap items-baseline">
+                    <div class="flex-auto p-6">
 
+                      <div class="flex flex-wrap items-baseline">
+                        <h4 class=" mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                          Perfil Profesional:
+                        </h4>
+                        <h5 class="font-normal text-gray-700 dark:text-gray-400">
+                          Ingeniero Ambiental
+                          Especialista en Gerencia de Proyectos
+                          Especialista Tecnológico en Implementación de Sistemas de Información Geográfica
+                          Piloto Profesional de Aeronaves no Tripuladas - UAS/RPAS/DRONE
+                        </h5>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
 
-            <div class=" block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-              <div className="flex space-x-2 justify-center">
-                <div class="flex-none w-52 relative">
-                  <img src="default-avatar.png" alt="" class="relative inset-0 w-500 h-500 object-cover rounded-lg" loading="lazy" />
+            <div class="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+              <div className="flex flex-col md:flex-row">
+                <div className="md:w-1/4 md:order-1  flex-auto p-6">
+                  <div class="flex-none w-52 ">
+                    <img src="default-avatar.png" alt="" class="relative inset-0 w-500 h-500 object-cover rounded-lg" loading="lazy" />
+                  </div>
                 </div>
-                <div class="flex-auto p-6">
+                <div className="md:w-3/4 md:order-2">
                   <div class="flex flex-wrap items-baseline">
-                    <h4 class=" mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      Title Three
-                    </h4>
-                    <h5 class="font-normal text-gray-700 dark:text-gray-400">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium id nunc hendrerit malesuada. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer eget ullamcorper diam. Duis ut nulla molestie, bibendum eros ac, elementum sem. Fusce viverra velit quis elit laoreet mollis. Quisque ultricies odio in nisi ultricies, ut mollis elit varius. Etiam ac viverra dui, in semper mauris. Quisque eget quam dolor. Suspendisse consequat leo at lectus dapibus facilisis. In rhoncus lectus sed mauris elementum consectetur. Maecenas ultrices scelerisque nibh, nec scelerisque tellus hendrerit at. Mauris luctus, orci id laoreet vestibulum.
-                    </h5>
+                    <div class="flex-auto p-6">
+                      <div class="flex flex-wrap items-baseline">
+                        <h4 class=" mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                          Title Three
+                        </h4>
+                        <h5 class="font-normal text-gray-700 dark:text-gray-400">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium id nunc hendrerit malesuada. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer eget ullamcorper diam. Duis ut nulla molestie, bibendum eros ac, elementum sem. Fusce viverra velit quis elit laoreet mollis. Quisque ultricies odio in nisi ultricies, ut mollis elit varius. Etiam ac viverra dui, in semper mauris. Quisque eget quam dolor. Suspendisse consequat leo at lectus dapibus facilisis. In rhoncus lectus sed mauris elementum consectetur. Maecenas ultrices scelerisque nibh, nec scelerisque tellus hendrerit at. Mauris luctus, orci id laoreet vestibulum.
+                        </h5>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
