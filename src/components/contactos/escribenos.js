ef5abec00f372e0c229f21cf90c659190aab6109
DAVIDTS0286 <67129995+DAVIDTS0286@users.noreply.github.com>
2023-04-16 13:46:44 -0500

    Pie de pagina

--- src/components/contactos/escribenos.js
+++ src/components/contactos/escribenos.js
@@ -31,11 +31,19 @@ const FloatingButton = () => {
 
     return (
 
-        <Link to="/contactos#form" className={`btn-flotante relative inline-block px-4 py-2 font-medium group${isScrolled ? " btn-flotante-scrolled" : ""}${isScrolled && window.innerHeight + window.scrollY >= document.body.clientHeight ? " btn-flotante-bottom" : ""}`}>
-            <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-lime-800 group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-            <span class="absolute inset-0 w-full h-full bg-white border-2 border-lime-800 group-hover:bg-lime-600"></span>
-            <span class="relative text-black group-hover:text-white">Escríbenos</span>
-        </Link>
+        <Link
+  to="/contactos#form"
+  className={`btn-flotante relative inline-block px-4 py-2 font-medium group${isScrolled ? " btn-flotante-scrolled" : ""}${isScrolled && window.innerHeight + window.scrollY >= document.body.clientHeight ? " btn-flotante-bottom" : ""}`}
+  style={{ fontSize: "1.2rem" }} // Este estilo se aplica a todos los tamaños de pantalla
+>
+  <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-lime-800 group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+  <span class="absolute inset-0 w-full h-full bg-white border-2 border-lime-800 group-hover:bg-lime-600"></span>
+  <span class="relative text-black group-hover:text-white text-sm">Escríbenos</span>
+</Link>
+
+
+
+
 
 
 
